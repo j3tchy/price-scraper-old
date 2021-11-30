2256d3e8af4c66d2e277acef23dccf506e21db08
Jermaine <jgyekye@gmail.com>
2021-11-30 22:30:20 +0000

    style(root): added logo and basic structure

---
+++ ui/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header filter drop-shadow-sm bg-white flex">
+            <div className="pl-6 flex items-center text-gray-500">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-14a3 3 0 00-3 3v2H7a1 1 0 000 2h1v1a1 1 0 01-1 1 1 1 0 100 2h6a1 1 0 100-2H9.83c.11-.313.17-.65.17-1v-1h1a1 1 0 100-2h-1V7a1 1 0 112 0 1 1 0 102 0 3 3 0 00-3-3z" clip-rule="evenodd" />
+                </svg>
+                <span className="uppercase font-sans font-bold text-lg pl-2">Price Scraper</span>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ ui/src/routes/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header/Header";
+
+const App = () => {
+  return (
+    <div className="bg-gray-50 h-screen">
+      <Header />
+    </div>
+  )
+}
+
+export default App
